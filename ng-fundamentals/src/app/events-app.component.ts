5239f74d4de492c3d67bf9d8ea1a2407eb467706
Aiyappa MA <aiyappa.ma@in.abb.com>
2021-02-04 13:30:25 +0530

    code updated ch02 complete

---
+++ ng-fundamentals/src/app/events-app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template:`
+  <h1>Hello world<h1>
+  <img src="/assets/images/basic-shield.png">
+  `, //path relative to index.html, path to assets also present in angular.json. allows webpack to include in our app bundle
+  styleUrls: ['./app.component.css']
+})
+export class EventsAppComponent {
+  title = 'app';
+}
