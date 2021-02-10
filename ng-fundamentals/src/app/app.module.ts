a35d2b51941049781debb7a6b20d7135a8ce120a
Aiyappa MA <aiyappa.ma@in.abb.com>
2021-02-10 13:18:22 +0530

    Organizing exports with barrels

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -2,18 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import{ RouterModule} from '@angular/router';
 import { NgModule } from '@angular/core';
 
+import{
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index'
+
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import{ EventThumbnailComponent} from './events/event-thumbnail.component';
 import{ NavbarComponent} from './nav/navbar.component';
-import{ EventService } from './events/shared/event.service'
 import{ ToastrService } from './common/toastr.service'
-import{ EventDetailsComponent } from './events/event-details/event-details.component'
-import{ CreateEventComponent } from './events/create-event.component'
 import { Error404Component } from './errors/404.component'
 import { appRoutes } from './routes';
-import {EventRouteActivator } from './events/event-details/event-route-activator.service'
-import {EventListResolver} from './events/events-list-resolver.service'
 
 @NgModule({
   declarations: [
