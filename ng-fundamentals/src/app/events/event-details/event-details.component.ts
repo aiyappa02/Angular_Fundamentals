a1d090af2aa463337ebd757efd48d7540bf152f8
Aiyappa MA <aiyappa.ma@in.abb.com>
2021-02-15 10:01:50 +0530

    filtering data

--- ng-fundamentals/src/app/events/event-details/event-details.component.ts
+++ ng-fundamentals/src/app/events/event-details/event-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { EventService } from '../shared/event.service'
 import { ActivatedRoute } from '@angular/router'
 import { IEvent,ISession } from '../shared/index'
@@ -12,9 +12,11 @@ import { IEvent,ISession } from '../shared/index'
     `]
 })
 
-export class EventDetailsComponent {
+export class EventDetailsComponent implements OnInit{
     event:IEvent
     addMode:boolean
+    filterBy : string = 'all'; // will not match the casing in display
+
     constructor( private eventService : EventService, private route :ActivatedRoute){
 
     }
--- ng-fundamentals/src/app/events/event-details/session-list.component.ts
+++ ng-fundamentals/src/app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnChanges } from '@angular/core'
 import { ISession} from "../shared/index"
 
 @Component( {
@@ -6,6 +6,25 @@ import { ISession} from "../shared/index"
     templateUrl : './session-list.component.html'
 })
 
-export class SessionListComponent{
+export class SessionListComponent implements OnChanges{
     @Input() sessions : ISession[]
+    @Input() filterBy : string;
+    visibleSessions : ISession[] = [];
+
+    ngOnChanges(){ //it is run when any of the input class properties changes
+        if(this.sessions){ //if we actually have sessions set
+            this.filterSessions(this.filterBy);
+        }
+    }
+
+    filterSessions(filter){
+        if(filter === 'all'){
+            this.visibleSessions = this.sessions.slice(0);
+        }
+        else{
+            this.visibleSessions = this.sessions.filter(session => {
+                return session.level.toLocaleLowerCase() === filter;
+            })
+        }
+    }
 }
