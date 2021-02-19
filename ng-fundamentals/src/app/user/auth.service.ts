0087d09124ef609d537a873e7947ee9c90b47c9c
Aiyappa MA <aiyappa.ma@in.abb.com>
2021-02-19 15:22:13 +0530

    Integrating authentication with the server

--- ng-fundamentals/src/app/user/auth.service.ts
+++ ng-fundamentals/src/app/user/auth.service.ts
@@ -1,15 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core' 
 import{ IUser } from './user.model'
+import {tap, catchError} from 'rxjs/operators';
+import { of } from 'rxjs';
+
 @Injectable()
 export class AuthService {
     currentUser : IUser
+
+    constructor(private http : HttpClient){}
+
     loginUser(userName : string, password: string){
-        this.currentUser = {
-            id:1,
-            userName : userName,
-            firstName : 'John',
-            lastName : 'Papa'
-        }
+
+        let loginInfo = {username : userName, password : password };
+        let options = { headers : new HttpHeaders({ 'Content-Type' : 'application/json'})};
+
+        return this.http.post('/api/login', loginInfo, options)
+            .pipe(tap(data => {
+                this.currentUser = <IUser>data['user']
+            }))
+            .pipe(catchError( err => {
+                return of(false)
+            }))
+        // this.currentUser = {
+        //     id:1,
+        //     userName : userName,
+        //     firstName : 'John',
+        //     lastName : 'Papa'
+        // }
     }
 
     isAuthenticated() {
