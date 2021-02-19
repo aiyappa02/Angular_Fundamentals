0a62de9579786799b6de92ff34f5d3c030e51f5c
Aiyappa MA <aiyappa.ma@in.abb.com>
2021-02-19 16:02:40 +0530

    persisting authentication status across page ref

--- ng-fundamentals/src/app/events-app.component.ts
+++ ng-fundamentals/src/app/events-app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from  './user/auth.service'
 
 @Component({
   selector: 'events-app',
@@ -14,5 +15,9 @@ import { Component } from '@angular/core';
   //styleUrls: ['./app.component.css']
 })
 export class EventsAppComponent {
-  
+    constructor(private auth : AuthService) {}
+
+    ngOnInit() {
+      this.auth.checkAuthenticationStatus();
+    }
 }
